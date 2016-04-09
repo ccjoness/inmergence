c716622eae733ffca4c30c9a2c78ebd6bcf75cab
chris <asdfasd@afasdf.com>
2016-04-09 13:43:34 -0700

    starting to build up app. added models

--- inmergence_app/settings.py
+++ inmergence_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'siteapps'
 ]
 
 MIDDLEWARE_CLASSES = [
--- siteapps/models.py
+++ siteapps/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+def file_upload_helper(instance, filename):
+    return '{0}/{1}'.format(instance.organization.name, filename)
+
+
+class InmergenceUser(models.Model):
+    user = models.ForeignKey(User)
+    dob = models.CharField(max_length=100)
+    sex = models.CharField(max_length=100)
+    prof = models.CharField(max_length=100)
+
+
+class Organization(models.Model):
+    user = models.ForeignKey(InmergenceUser)
+    name = models.CharField(max_length=250)
+    id = models.CharField(max_length=250, primary_key=True)
+
+
+class Document(models.Model):
+    organization = models.ForeignKey(Organization)
+    id = models.CharField(max_length=250, primary_key=True)
+    name = models.CharField(max_length=250)
+    file = models.FileField(upload_to=file_upload_helper)

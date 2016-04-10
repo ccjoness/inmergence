ee2fc666f1af489e2a08c3196f9fefb95700bfcf
chris <asdfasd@afasdf.com>
2016-04-10 14:43:32 -0700

    text highlighting is working

--- siteapps/scratch.py
+++ siteapps/scratch.py
@@ -1,13 +1,37 @@
-import cloudconvert # pip install cloudconvert
+from bs4 import BeautifulSoup
+import os
+import re
 
-api = cloudconvert.Api('HKi3ooM6JI_caLFxb90B5lYONnKmoGjGNZ8R3Ozx22XB9pJJDzk1wx9fBgJEDqu-s_gmwWc1R31h_YPABQOZjw')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-process = api.convert({
-    "input": "upload",
-    "email": "true",
-    "inputformat": "pdf",
-    "outputformat": "html",
-    "file": open('../CisnerosSandra-Eleven.pdf', 'rb')
-})
-process.wait()
-process.download('../templates/')
+pz = 'C:\\projects\\inmergence_app\\templates\\PDX_Code_Guild\\CisnerosSandra-Eleven.zip'
+
+with open(pz.replace('zip', 'html'), 'r+', encoding="utf8") as html:
+    with open(pz[:-4] + '-rendered.html', 'a', encoding="utf8") as wrt:
+        wrt.write('{% extends "base.html" %}')
+        wrt.write('{% block content %}')
+        for line in html.readlines()[37:-2]:
+            wrt.write(line)
+        wrt.write('{% endblock %}')
+
+# output = re.sub(r'(.*<body bgcolor="#A0A0A0" vlink="blue" link="blue">)',
+#                 string='{% extends "base.html" %}\n{% block content %}\n',
+#                 repl="\s"
+#                 )
+# out_str = ",".join(output)
+# with open("output.txt", "w") as outp:
+#     outp.write(out_str)
+# m = re.search(r'<h1>Title</h1>.*?<h1>', open(pz.replace('zip', 'html'), 'r+', encoding="utf8"), re.DOTALL)
+# s = m.start()
+# e = m.end() - len('<h1>')
+# target_html = html[s:e]
+# soup = BeautifulSoup(html, "html.parser")
+
+# html = ""
+# for tag in soup.find("body").next_siblings:
+#     if tag.name == "body":
+#         break
+#     else:
+#         html += tag
+#
+# print(html)
--- siteapps/views.py
+++ siteapps/views.py
@@ -3,6 +3,8 @@ import cloudconvert
 from .models import Document, Organization, InmergenceUser
 import uuid
 import os
+import zipfile
+import re
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
@@ -10,25 +12,50 @@ from django.shortcuts import render
 from .forms import UserForm, UserProfileForm
 #We added this to handle the HttpResponse in this particular manner.
 from django.http.response import HttpResponse
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-def clean_html(path, doc_name):
-    doc_name = str(doc_name).replace('/', '\\')
-    print(path + doc_name[:-3] + 'txt')
-    f = open(path + doc_name[:-3] + 'txt', 'r+', encoding='utf8')
-    x = open(path + doc_name[:-3] + 'html', 'w', encoding='utf8')
+def clean_html(path, org_name, doc_name):
+    s = '\\'
+    # path to zip file
+    pz = path + org_name + s + doc_name
+    # path to location zip files are loaded to
+    pt = path + org_name + s
+    fh = open(pz, 'rb')
+    z = zipfile.ZipFile(fh)
+    for name in z.namelist():
+        outpath = pt
+        z.extract(name, outpath)
+    fh.close()
 
-    header1 = "{% extends 'base.html' %}\n"
-    header2 = "{% block content %}\n"
-    footer1 = "\n{% endblock %}"
-    x.write(header1)
-    x.write(header2)
-    for line in f.readlines():
-        x.write(line)
-    x.write(footer1)
+    import os, shutil
+    folder = pt
+    for the_file in os.listdir(folder):
+        if str(the_file)[-4:] == "html":
+            pass
+        else:
+            file_path = os.path.join(folder, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
+
+    with open(pz.replace('zip', 'html'), 'r+', encoding="utf8") as html:
+        with open(pz[:-4] + '-rendered.html', 'a', encoding="utf8") as wrt:
+            wrt.write('{% extends "base.html" %}\n')
+            wrt.write('{% block content %}\n')
+            for line in html.readlines()[37:-2]:
+                wrt.write(line)
+            wrt.write('{% endblock %}\n')
 
 
 def index(request):
+    m = BASE_DIR + "\\media\\"
+    t = BASE_DIR + "\\templates\\"
     if request.method == "POST":
         usr = InmergenceUser.objects.get(user=request.user)
         org = Organization.objects.get(user=usr)
@@ -48,17 +75,18 @@ def index(request):
             "input": "upload",
             "email": "false",
             "inputformat": fformat,
-            "outputformat": "txt",
+            "outputformat": "html",
             "preset": "ytfLaX3GgM",
-            "file": open('C:\\projects\\inmergence_app\\media\\' + str(doc.file), 'rb')
+            "file": open(m + str(doc.file), 'rb')
         })
 
-        path = 'C:\\projects\\inmergence_app\\templates\\' + org.name.replace(' ', '_') + '\\'
+        path = t + org.name.replace(' ', '_') + '\\'
         if not os.path.exists(path):
             os.mkdir(path)
         process.wait()
         process.download(path)
-        clean_html('C:\\projects\\inmergence_app\\templates\\', doc.file)
+        real_file_name_to_zip = str(doc.file).replace(str(org.name).replace(" ", "_") + "/", "").replace('pdf', 'zip')
+        clean_html(t, str(org.name).replace(' ', '_'), real_file_name_to_zip)
         context = {}
         return render(request, 'index.html', context)
     else:
@@ -81,7 +109,7 @@ def doc(request, org, docu):
     doc = Document.objects.get(id=docu)
     loca = str(doc.file)[:-3] + 'html'
     print(loca)
-    return render(request, str(doc.file)[:-3] + 'html', {})
+    return render(request, str(doc.file)[:-4] + '-rendered.html', {})
 
 
 # Create your views here.
